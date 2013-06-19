30834dfa4a9a1d1a5b8419d72b9fcaeba2d942bd
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-06-19 13:38:42 +0700

    Add lint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-mocha-cli');
   grunt.loadNpmTasks('grunt-bg-shell');
 
+  grunt.registerTask('lint', ['jshint']);
+
   grunt.registerTask('test', ['jshint', 'bgShell:dropDatabase', 'mochacli',
     'bgShell:dropDatabase']);
 };
